34ede118065a85b24f047c36e3ce8d6e5d700d77
tuliocv <130805106+tuliocv@users.noreply.github.com>
2026-03-02 15:19:01 -0300

    Fix formatting in rental car prompt text

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ EXS = [
     {
         "id": "Ex 9",
         "title": "Carro alugado",
-        "prompt": "Ex 9: Escreve um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro ficou alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado."
+        "prompt": "Ex 9: Escreve um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro ficou alugado. Calcule o preço a pagar, sabendo que o carro custa  60,00 por dia e  0,15 por km rodado."
     },
     {
         "id": "Ex 10",
